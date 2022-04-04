cfb374f68336ad064836788c338fa285c85b89bc
yanko-av <ayyankovsky@gmail.com>
2022-04-04 11:31:50 +0300

    add getters/setters

--- src/company/Box.java
+++ src/company/Box.java
@@ -4,11 +4,69 @@ public class Box {
     int a;
     int b;
     int c;
+    String colour;
+    String material;
 
     Box(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    Box(int a, int b, int c, String colour, String material) {
+        this(a, b, c);
+        this.colour = colour;
+        this.material = material;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        if (a <= 0) {
+            System.out.println("Incorrect size A");
+//            return;
+        } else this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        if (b <= 0) {
+            System.out.println("Incorrect size B");
+//            return;
+        } else this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        if (c <= 0) {
+            System.out.println("Incorrect size C");
+//            return;
+        } else this.c = c;
+    }
+
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
     }
 
     public int volume() {
