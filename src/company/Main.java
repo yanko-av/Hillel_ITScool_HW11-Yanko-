2fbedd83b4fcd2254258b1314d8fd5cbdf41db6b
yanko-av <ayyankovsky@gmail.com>
2022-04-04 11:36:24 +0300

    add getters/setters

--- src/company/Main.java
+++ src/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
         System.out.println();
 
         box1.setB(-2);
-        box1.setA(2);
+        box2.setA(2);
         System.out.println("Volume of box 1 is -  " + box1.volume());
         System.out.println("Volume of box 2 is -  " + box2.volume());
 
